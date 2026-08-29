595f8870bd3c74954bbcd79f21fce2a9fd7228ab
Suraj Budakoti <surajpb338@gmail.com>
2026-08-29 17:48:12 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     String str = "        ";
+        System.out.println(str.isBlank());
+        System.out.println(str.isEmpty());
+        System.out.println(str.isEmpty());
+    }
+}
