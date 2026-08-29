f614f676d3f006a3782c7cd4459775c5f2d8a8f2
Suraj Budakoti <surajpb338@gmail.com>
2026-08-29 21:43:38 +0200

    Add wordcounter() and getTotalWords().

--- src/WordManager.java
+++ src/WordManager.java
@@ -12,11 +12,14 @@ public class WordManager {
 
     public boolean isStop(String userInput) {
         boolean stop = true;
+
         if (!userInput.equalsIgnoreCase("stop")){
             totalLines++;
             characterCounter(userInput);
+            wordCounter(userInput);
             stop = false;
         }
+
         return stop;
     }
 
@@ -30,5 +33,14 @@ public class WordManager {
 
     public int getTotalCharacters() {
         return totalCharacter;
+    }
+
+    public void wordCounter(String userInput) {
+        String [] arrayWithWords = userInput.trim().split("\\s+");
+        totalWords += arrayWithWords.length;
+    }
+
+    public int getTotalWords() {
+        return totalWords;
     }
 }
