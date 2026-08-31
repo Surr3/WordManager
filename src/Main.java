1bec0455454dad5835a18ab371908859eb8174f8
Suraj Budakoti <surajpb338@gmail.com>
2026-08-31 13:29:26 +0200

    Add comments to clarify functions.

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,14 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Write a word/text (type \"stop\" to exit):\n> ");
+
+        // Reads input while user hasn't typed "stop"
         while (wordManager.isNotStop(scan.nextLine())) {
             System.out.print("Write another word/text (\"stop\" to exit): \n> ");
         }
         scan.close();
 
+        // Outputs result
         System.out.println("\nTotal characters:  " + wordManager.getTotalCharacters());
         System.out.println("Total lines:       " + wordManager.getTotalLines());
         System.out.println("Total words:       " + wordManager.getTotalWords());
--- src/WordManager.java
+++ src/WordManager.java
@@ -1,55 +1,84 @@
 public class WordManager {
 
-    private int totalCharacter, totalLines, totalWords;
-    private int longestWordsTotCharacters = 0;
+    private int totalCharacter;
+    private int totalLines;
+    private int totalWords;
+
+    private int longestWordsTotalCharacters;
     private String longestWords = "You didn't enter anything!";
 
+    // Checks if user has typed "stop".
     public boolean isNotStop(String userInput) {
-        boolean stop = false;
-
+        boolean notStopYet = false;
+        /*
+         * Process the input if user hasn't typed "stop"
+         * Add 1 to totalLines, even if it's an empty input.
+         * Every input creates a line except "stop".
+         */
         if (!userInput.equalsIgnoreCase("stop")) {
             totalLines++;
+            /*
+             * Processes the input if user has typed something (including blank space).
+             * Skip this part if user only hit enter.
+             */
             if (!userInput.isEmpty()) {
-                characterCounter(userInput);
-                wordCounter(userInput);
+                countCharacters(userInput);
+                countWords(userInput);
             }
-            stop = true;
+
+            notStopYet = true;
         }
 
-        return stop;
+        return notStopYet;
+    }
+
+    public void countCharacters(String userInput) {
+        totalCharacter += userInput.length();
+    }
+
+    public void countWords(String userInput) {
+        /*
+         * Splits words from user input into an array while removing all blank spaces.
+         * Blank spaces are not considered words.
+         */
+        String[] arrayWithWords = userInput.trim().split("\\s+");
+        totalWords += arrayWithWords.length;
+
+        //Uses the same array with words, as previous, to find the longest word(s).
+        findLongestWords(arrayWithWords);
+    }
+
+    public void findLongestWords(String[] arrayWithWords) {
+        // Iterates through every word
+        for (String currentWord : arrayWithWords) {
+            /*
+             * Assigns currentWord to longestWords if currentWord has more characters.
+             * Then assigns length of currentWord to longestWordsTotCharacters for next iteration.
+             */
+            if (currentWord.length() > longestWordsTotalCharacters) {
+                longestWordsTotalCharacters = currentWord.length();
+                longestWords = "\"" + currentWord.toLowerCase() + "\"";
+            }
+            /*
+             * Adds currentWord to longestWords if currentWord is equally long to longestWords (longestWordsTotCharacters.length()).
+             * Also checks that currentWord is not already in longestWords to prevent duplicates.
+             */
+            else if (currentWord.length() == longestWordsTotalCharacters && !currentWord.isEmpty() && !longestWords.contains(currentWord.toLowerCase())) {
+                longestWords = longestWords + ", " + "\"" + currentWord.toLowerCase() + "\"";
+            }
+        }
     }
 
     public int getTotalLines() {
         return totalLines;
     }
 
-    public void characterCounter(String userInput) {
-        totalCharacter += userInput.length();
-    }
-
     public int getTotalCharacters() {
         return totalCharacter;
     }
 
-    public void wordCounter(String userInput) {
-        String[] arrayWithWords = userInput.trim().split("\\s+");
-        totalWords += arrayWithWords.length;
-        longestWords(arrayWithWords);
-    }
-
     public int getTotalWords() {
         return totalWords;
-    }
-
-    public void longestWords(String[] arrayWithWords) {
-        for (String currentWord : arrayWithWords) {
-            if (currentWord.length() > longestWordsTotCharacters) {
-                longestWordsTotCharacters = currentWord.length();
-                longestWords = "\"" + currentWord.toLowerCase() + "\"";
-            } else if (currentWord.length() == longestWordsTotCharacters && !currentWord.isEmpty() && !longestWords.contains(currentWord)) {
-                longestWords = longestWords + ", " + "\"" + currentWord.toLowerCase() + "\"";
-            }
-        }
     }
 
     public String getLongestWords() {
